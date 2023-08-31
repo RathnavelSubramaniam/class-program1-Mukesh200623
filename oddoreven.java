2de513146835a720e676463eacf26cfa7d6ead8c
Mukesh200623 <142876342+Mukesh200623@users.noreply.github.com>
2023-08-31 10:47:48 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,14 @@
+class oddoreven {
+public static void main (string args[]){
+int num=3;
+if(num%2!=0)
+System.out.println("the given number is ODD");
+  else
+System.out.println("the given number is EVEN");
+}
+}
+
+
+
+
+
